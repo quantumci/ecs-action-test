08f0f91c292affbcc3668641d5c32aa364fb7b60
pradipbabar <pradipbaba13@gmail.com>
2023-10-10 13:24:49 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,17 @@ config2_file="/workspace/Platform_Infra/env/platform.config"
 sed -i "s/^bucket=\"[^\"]*\"/bucket=\"$new_bucket\"/" "$config1_file"
 sed -i "s/^region=\"[^\"]*\"/region=\"$new_region\"/" "$config2_file"
 
+# check config file not for production use
+cat $config1_file
+cat $config2_file
+
 # Set AWS Creds 
 export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
+
+# check aws creds not for production use
+echo "$AWS_ACCESS_KEY_ID"
+echo "$AWS_SECRET_ACCESS_KEY"
 
 cd /workspace/Base_Infra/
 terraform init -backend-config="./env/base.config"
