dcaf11948bac4b135bc05b89197f6d7085b0c39e
pradipbabar <pradipbaba13@gmail.com>
2023-10-10 13:45:00 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,24 +10,26 @@ sed -i "s/^bucket=\"[^\"]*\"/bucket=\"$INPUT_REMOTE_BUCKET_NAME\"/" "$config2_fi
 sed -i "s/^region=\"[^\"]*\"/region=\"$INPUT_REMOTE_BUCKET_REGION\"/" "$config2_file"
 
 # check config file not for production use
-cat $config1_file
-cat $config2_file
+cat $config1_file;
+cat $config2_file;
 
 # Set AWS Creds 
-export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY;
+export AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET_ACCESS_KEY;
 
 # check aws creds not for production use
-echo "$AWS_ACCESS_KEY_ID"
-echo "$AWS_SECRET_ACCESS_KEY"
+echo "$AWS_ACCESS_KEY_ID";
+echo "$AWS_SECRET_ACCESS_KEY";
 
 cd /workspace/Base_Infra/
-terraform init -backend-config="./env/base.config"
-terraform plan -var-file="$INPUT_BASE_CONF_VAR"
-terraform $INPUT_ACTION -var-file="$INPUT_BASE_CONF_VAR"
+ls;
+# terraform init -backend-config="./env/base.config"
+# terraform plan -var-file="$INPUT_BASE_CONF_VAR"
+echo "terraform $INPUT_ACTION -var-file='$INPUT_BASE_CONF_VAR'"
 
 cd /workspace/Platform_Infra/
-terraform init -backend-config="./env/platform.config"
-terraform plan -var-file="$INPUT_PLATFORM_CONF_VAR"
-terraform $INPUT_ACTION -var-file="$INPUT_PLATFORM_CONF_VAR"
+ls;
+# terraform init -backend-config="./env/platform.config"
+# terraform plan -var-file="$INPUT_PLATFORM_CONF_VAR"
+echo "terraform $INPUT_ACTION -var-file='$INPUT_PLATFORM_CONF_VAR'"
 
