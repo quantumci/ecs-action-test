c1a13c2190030f65b1b857001f552da0a556b268
pradipbabar <pradipbaba13@gmail.com>
2023-10-10 13:39:11 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 # Update the values in the config file
 config1_file="/workspace/Base_Infra/env/base.config"
 config2_file="/workspace/Platform_Infra/env/platform.config"
 
-sed -i "s/^bucket=\"[^\"]*\"/bucket=\"$new_bucket\"/" "$config1_file"
-sed -i "s/^region=\"[^\"]*\"/region=\"$new_region\"/" "$config2_file"
+sed -i "s/^bucket=\"[^\"]*\"/bucket=\"$INPUT_REMOTE_BUCKET_NAME\"/" "$config1_file"
+sed -i "s/^region=\"[^\"]*\"/region=\"$INPUT_REMOTE_BUCKET_REGION\"/" "$config1_file"
+sed -i "s/^bucket=\"[^\"]*\"/bucket=\"$INPUT_REMOTE_BUCKET_NAME\"/" "$config2_file"
+sed -i "s/^region=\"[^\"]*\"/region=\"$INPUT_REMOTE_BUCKET_REGION\"/" "$config2_file"
 
 # Set AWS Creds 
 export AWS_ACCESS_KEY_ID=$INPUT_AWS_ACCESS_KEY
